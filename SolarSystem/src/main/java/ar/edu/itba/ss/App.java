ba6a7a8f7d63d8a29dcaf3fa6d69823c663bd8df
tinchovictory <martin@victory.com.ar>
2018-09-24 17:04:42 -0300

    simulation not workingW

--- SolarSystem/src/main/java/ar/edu/itba/ss/App.java
+++ SolarSystem/src/main/java/ar/edu/itba/ss/App.java
@@ -1,9 +1,32 @@
 package ar.edu.itba.ss;
 
+import java.awt.geom.Point2D;
+
 public class App {
     public static void main(String[] args) {
 
-        new Simulation(0.01, 0, 10).start();
+        new Simulation(10, 15, 10).start();
+/*
+        double minDistance = Double.POSITIVE_INFINITY;
+        int minL = 0;
+        int minV0 = 0;
 
+        for(int L = 0; L < 10000; L += 10) {
+            for(int v0 = 1; v0 < 20; v0++) {
+                System.out.println("-- Simulation with L: " + L + " and v0: " + v0 + " --");
+                Point2D distance = new Simulation(10, L, v0).start();
+
+                if((distance.getX() + distance.getY()) / 2 < minDistance) {
+                    minDistance = (distance.getX() + distance.getY()) / 2;
+                    minL = L;
+                    minV0 = v0;
+                }
+            }
+        }
+
+        System.out.println("Min L: " + minL);
+        System.out.println("Min v0: " + minV0);
+        System.out.println("Distance: " + minDistance);
+*/
     }
 }
--- SolarSystem/src/main/java/ar/edu/itba/ss/Simulation.java
+++ SolarSystem/src/main/java/ar/edu/itba/ss/Simulation.java
@@ -37,7 +37,7 @@ public class Simulation {
         beemanCalculator = new BeemanCalculator(dTime, fc);
     }
 
-    public void start() {
+    public Point2D start() {
         double time = 0, minDistanceJupiter = Double.POSITIVE_INFINITY, minDistanceSaturn = Double.POSITIVE_INFINITY;
         boolean done = false;
 
@@ -46,7 +46,9 @@ public class Simulation {
 
         Set<Particle> planetsParticles = new HashSet<Particle>(planets.values());
 
-        while(!done && time < 100000) {
+        int iter = 0;
+
+        while(!done && iter < 100000) {
 
             // Update planets position
             beemanCalculator.update(planetsParticles);
@@ -66,16 +68,22 @@ public class Simulation {
                 minDistanceSaturn = distanceToSaturn;
             } else {
                 // Stop when the voyager gets away from saturn
-                System.out.println("done" + distanceToJupiter);
-                System.out.println("done" + distanceToSaturn);
-                done = true;
+                //System.out.println("done" + distanceToJupiter);
+                //System.out.println("done" + distanceToSaturn);
+                //done = true;
             }
 
-            System.out.println("Min distance to jupiter: " + minDistanceJupiter);
-            System.out.println("Min distance to saturn: " + minDistanceSaturn);
-
+            //System.out.println("Distance to jupiter: " + distanceToJupiter);
+            //System.out.println("Distance to saturn: " + distanceToSaturn);
             time += dTime;
+            iter++;
         }
+
+        //System.out.println("Min distance to jupiter: " + minDistanceJupiter);
+        //System.out.println("Min distance to saturn: " + minDistanceSaturn);
+
+
+        return new Point2D.Double(minDistanceJupiter, minDistanceSaturn);
     }
 
     private Point2D getVoyagerInitialPosition(double L) {
@@ -124,11 +132,10 @@ public class Simulation {
 
         Map<String, Particle> planets = new HashMap<String, Particle>();
 
-        // dummy loading to avoid null pointer exeption
-        planets.put("sun", new Particle(0,0,0,0,0,1000));
-        planets.put("earth", new Particle(0,1,1,0,0,1000));
-        planets.put("jupiter", new Particle(0,2,3,0,0,1000));
-        planets.put("saturn", new Particle(0,3,3,0,0,1000));
+        planets.put("sun", new Particle(0, 3.541021921578323E+05,-6.578268133977889E+05,1.396466697359526E-02,4.212108884730581E-05,1988500E+24));
+        planets.put("earth", new Particle(1,1.443040359985483E+08,-4.566821691926755E+07,8.429276455862507,2.831601955976786E+01,5.97219E+24));
+        planets.put("jupiter", new Particle(2,1.061950341671551E+08,7.544955348409320E+08,-1.309157032053854E+01,2.424744678419164,1898.13E+24));
+        planets.put("saturn", new Particle(3,-1.075238877886715E+09,8.538222924091074E+08,-6.527515746018062,-7.590526046562251,5.6834E+26));
 
         return planets;
     }
